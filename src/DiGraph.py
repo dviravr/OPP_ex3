4b17b9cdd701bf9b0a0660e5cb72fb39f733b359
DavidHarlap <adavid12@gmail.com>
2021-01-12 02:14:59 +0200

    Adding comments and deleting unnecessary stuff

--- src/DiGraph.py
+++ src/DiGraph.py
@@ -17,35 +17,38 @@ class DiGraph(GraphInterface):
         self._inEdge: dict = {}
 
     def v_size(self) -> int:
+        # Returns the number of edges in this graph
         if self._graphNodes is None:
             return 0
         return len(self._graphNodes)
 
     def e_size(self) -> int:
+        # Returns the number of edges in this graph
         return self._edgeCount
 
     def get_all_v(self) -> dict:
+        # return a dictionary of all the nodes in the Graph
         return self._graphNodes
 
     def all_in_edges_of_node(self, id1: int) -> dict:
-        if self._graphNodes is None or self._graphNodes.get(id1) is None:
+        # check if id1 in the graph, if true return a dictionary of all the nodes connected into id1
+        if self._graphNodes.get(id1) is None:
             return {}
         return self._inEdge.get(id1)
 
     def all_out_edges_of_node(self, id1: int) -> dict:
-        if self._graphNodes is None or self._graphNodes.get(id1) is None:
+        # # check if id1 in the graph, if true return a dictionary of all the nodes connected from id1
+        if self._graphNodes.get(id1) is None:
             return {}
         return self._edges.get(id1)
 
     def get_mc(self) -> int:
+        # Returns the current version of this graph, if the graph change the MC should be MC++
         return self._modeCount
 
     def add_edge(self, id1: int, id2: int, weight: float) -> bool:
         # cannot have negative weight
         if weight < 0.0:
-            return False
-        # the graph is empty
-        if self._graphNodes is None:
             return False
         # id1 or id2 don't exist
         elif id1 not in self._graphNodes or id2 not in self._graphNodes or id1 == id2:
@@ -61,35 +64,34 @@ class DiGraph(GraphInterface):
             return True
 
     def add_node(self, node_id: int, pos: tuple = None) -> bool:
+        # IF THE NODE ALREADY EXIST SHOULD DO NOTHING.
         if node_id in self._graphNodes:
             return False
         self._modeCount += 1
         self._graphNodes[node_id] = Node(node_id, pos)
-        # pos if pos is not None else None)
-
         self._edges[node_id] = {}
         self._inEdge[node_id] = {}
-
         return True
 
     def remove_node(self, node_id: int) -> bool:
+        # the method removes a node from the graph
         if node_id not in self._graphNodes:
             return False
         self._modeCount += 1
         for x in self._edges[node_id]:
             del (self._inEdge[x][node_id])
-            self._edgeCount -=1
+            self._edgeCount -= 1
         for x in self._inEdge[node_id]:
             del (self._edges[x][node_id])
-            self._edgeCount -=1
+            self._edgeCount -= 1
         del (self._graphNodes[node_id])
         del (self._edges[node_id])
         del (self._inEdge[node_id])
         return True
 
     def remove_edge(self, node_id1: int, node_id2: int) -> bool:
-        if self._graphNodes is None:
-            return False
+        # removes an edge from the graph.
+        # the 2 next if check if the edge exist.
         if node_id1 not in self._graphNodes or node_id2 not in self._graphNodes:
             return False
         if node_id2 not in self.all_out_edges_of_node(node_id1):
@@ -101,6 +103,7 @@ class DiGraph(GraphInterface):
         return True
 
     def __dict__(self):
+        # TO STRING - used for save to json
         graph_nodes = self.get_all_v()
         json_nodes = []
         json_edges = []
