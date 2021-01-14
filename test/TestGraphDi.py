eba4dc03dc28e631453034295353a615a233c54d
DavidHarlap <adavid12@gmail.com>
2021-01-14 22:52:13 +0200

    fix import

--- test/TestGraphDi.py
+++ test/TestGraphDi.py
@@ -18,6 +18,7 @@ class TestGraphDi(unittest.TestCase):
         self.assertEqual(g.e_size(), 2)
 
     def test_empty_graph(self):
+        # All the situations that can happen in case the graph is empty
         g: GraphInterface = DiGraph()
 
         self.assertEqual(0, g.v_size())
@@ -32,16 +33,16 @@ class TestGraphDi(unittest.TestCase):
         g: GraphInterface = DiGraph()
         for n in range(5):
             g.add_node(n)
-        self.assertEqual(g.v_size(), 5)
+        self.assertEqual(g.v_size(), 5) # after add 5 nose
         g.remove_node(2)
-        self.assertEqual(g.v_size(), 4)
+        self.assertEqual(g.v_size(), 4) # after del 1 node
 
     def test_add_node(self):
         g: GraphInterface = DiGraph()
         g.add_node(0, (1, 1, 0))
         g.add_node(0, (2, 2, 0))
-        self.assertEqual(g.v_size(), 1)
-        self.assertEqual(g.get_mc(), 1)
+        self.assertEqual(g.v_size(), 1)  # add the same key and doesnt change
+        self.assertEqual(g.get_mc(), 1)  # MC dont change
         # check if the "new node" don't switch node 0
         self.assertEqual(g.get_all_v().get(0).get_pos(), (1, 1, 0))
 
@@ -51,26 +52,25 @@ class TestGraphDi(unittest.TestCase):
             g.add_node(n)
         g.add_edge(0, 1, 1)
         g.add_edge(0, 2, 1)
-        self.assertEqual(g.e_size(), 2)
+        self.assertEqual(g.e_size(), 2)  # after add 2 edge
         self.assertEqual(g.get_mc(), 6)
         # the next 2 edge should not count.
         g.add_edge(0, 1, 1)
         g.add_edge(0, 1, 2.5)
         self.assertEqual(g.e_size(), 2)
         self.assertEqual(g.get_mc(), 6)
-        # can not add edge between node to himself.
-        self.assertFalse(g.add_edge(0, 0, 1.2))
+        self.assertFalse(g.add_edge(0, 0, 1.2))  # can not add edge between node to himself.
 
     def test_remove_edge(self,):
         g: GraphInterface = DiGraph()
-        print(g.remove_edge(0,4))
+        print(g.remove_edge(0, 4))
         for n in range(4):
             g.add_node(n)
-        self.assertFalse(g.remove_edge(0, 1))
+        self.assertFalse(g.remove_edge(0, 1))  # edge dont exist
         g.add_edge(0, 1, 1)
         g.add_edge(0, 2, 1)
         g.remove_edge(0, 1)
-        self.assertEqual(1, g.e_size())
+        self.assertEqual(1, g.e_size())  # 2 - 1 = 1
 
     def test_remove_node(self):
         g: GraphInterface = DiGraph()
@@ -80,9 +80,9 @@ class TestGraphDi(unittest.TestCase):
         g.add_edge(0, 2, 1)
         g.add_edge(0, 3, 1.1)
         g.add_edge(1, 0, 4)
-        self.assertEqual(4, g.e_size())
+        self.assertEqual(4, g.e_size())  # after add 4 edge
         g.remove_node(0)
-        self.assertEqual(0, g.e_size())
+        self.assertEqual(0, g.e_size())  # all edge connect to node 0 so all need to remove
 
     def test_all_in_out_edges_of_node(self):
         g: GraphInterface = DiGraph()
@@ -93,9 +93,10 @@ class TestGraphDi(unittest.TestCase):
         g.add_edge(0, 3, 1.1)
 
         print("should be {1: 1, 2: 1, 3: 1.1} we get:", g.all_out_edges_of_node(0))
-        self.assertEqual({}, g.all_out_edges_of_node(1))
-        self.assertEqual({}, g.all_out_edges_of_node(14))
+        self.assertEqual({}, g.all_out_edges_of_node(1))  # only in edge to node 1, 0 out edge
         self.assertEqual({0: 1}, g.all_in_edges_of_node(1))
+        self.assertEqual({}, g.all_out_edges_of_node(14))  # node dont exist should be empty
+
 
     def test_get_MC(self):
         g: GraphInterface = DiGraph()
